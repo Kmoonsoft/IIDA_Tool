d4a1a7e31a9a1a27f8180515f66bc43e9034eb54
Lucky Kim <neoeeff@gmail.com>
2021-10-03 23:36:20 +0900

    release first

--- 미사용QtUtility/QtUtility.cpp
+++
@@ -1,5 +0,0 @@
-#include "QtUtility.h"
-
-QtUtility::QtUtility()
-{
-}
--- 미사용QtUtility/QtUtility.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "qtutility_global.h"
-
-class QTUTILITY_EXPORT QtUtility
-{
-public:
-    QtUtility();
-};
